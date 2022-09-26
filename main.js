919fff1660ae8c0c3a4e0b6575b2e1f4f8568615
太田紘彰 <otahiroaki@ootahiroshiakiranoMacBook-Air.local>
2022-09-26 11:46:27 +0900

    平行光源と画面リサイズの対応

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 //objectの作成
 //material
 const material = new THREE.MeshPhysicalMaterial({
-  color: "3c94d7",
+  color: "#3c94d7",
   metalness: 0.86,
   roughness: 0.37,
   flatShading: true,
@@ -53,4 +53,31 @@ mesh3.position.set(2, 0, -6);
 mesh4.position.set(5, 0, 3);
 
 scene.add(mesh1, mesh2, mesh3, mesh4);
-renderer.render(scene, camera);
+
+//Light
+const directionalLight = new THREE.DirectionalLight("#ffffff", 4);
+directionalLight.position.set(0.5, 1, 0);
+scene.add(directionalLight);
+
+//resize
+window.addEventListener("resize", ()=> {
+  //サイズのアップデート
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //カメラのアップデート
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  //レンダラーのアップデート
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(window.devicePixelRatio); 
+});
+
+//アニメーション
+const animate = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate);
+}
+
+animate();
